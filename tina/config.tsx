64e7c27b2f0a10fe1dce40a68742219a581dfac1
cubajastk-hue <cubajastk@gmail.com>
2026-02-01 13:05:36 +0100

    Update config.tsx

--- tina/config.tsx
+++ tina/config.tsx
@@ -1,12 +1,6 @@
 import { defineConfig } from "tinacms";
 import nextConfig from '../next.config'
 
-import Post from "./collection/post";
-import Global from "./collection/global";
-import Author from "./collection/author";
-import Page from "./collection/page";
-import Tag from "./collection/tag";
-
 const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   branch:
