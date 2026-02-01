f1bea1c5a85736bacdc5f5f31c9ddc5e52c4951d
cubajastk-hue <cubajastk@gmail.com>
2026-02-01 13:22:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import client from "@/tina/__generated__/client";
-import Layout from "@/components/layout/layout";
-import ClientPage from "./[...urlSegments]/client-page";
+import { client } from "../tina/__generated__/client";
 
-export const revalidate = 300;
-
-export default async function Home() {
-  const data = await client.queries.page({
-    relativePath: `home.mdx`,
-  });
+export default async function HomePage() {
+  // Tady říkáme Tině: "Dej mi všechny 'Zážitky'"
+  const zazitkyResponse = await client.queries.zazitekConnection();
+  const seznamZazitku = zazitkyResponse.data.zazitekConnection.edges;
 
   return (
-    <Layout rawPageData={data}>
-      <ClientPage {...data} />
-    </Layout>
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>📸 Moje Vzpomínky</h1>
+      <hr />
+      <div style={{ marginTop: "2rem" }}>
+        {seznamZazitku?.map((zazitek) => (
+          <div key={zazitek?.node?.id} style={{ marginBottom: "2rem", borderBottom: "1px solid #eee" }}>
+            <h2>{zazitek?.node?.title}</h2>
+            {/* Tady budeme později vykreslovat text a fotky */}
+            <p>Zážitek je připraven k naplnění!</p>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
